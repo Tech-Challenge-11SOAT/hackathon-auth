7395eb58b476b82da7a718b399c884712d6360fb
Pedro Argentati <pedro.argentatii@gmail.com>
2026-02-15 10:48:19 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/hackathon_auth/HackathonAuthApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackathon_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/hackathon_auth/HackathonAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackathon_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
